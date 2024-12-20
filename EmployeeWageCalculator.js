663f16b93126c167dff36423d7fff50e25c2b6dc
Nachiketha <nachiketh.k.c@brainsightai.com>
2024-12-20 15:47:03 +0530

    Total working days

--- EmployeeWageCalculator.js
+++ EmployeeWageCalculator.js
@@ -1,12 +1,29 @@
 console.log("Welcome To Employee Wage Calculator");
 
 
-let  getEmployeeAttendence = ()=> Math.floor(Math.random() * 10) % 2;
+let getEmployeeAttendence = () => Math.floor(Math.random() * 10) % 2;
 let totalDayEmployeePresent = 0;
+let checkNumberOfPartTime = 0;
+let checkNumberOfFullTime = 0;
+
 
 
 for (let i = 1; i <= 20; i++) {
 
     let employeeAttendence = getEmployeeAttendence();
     totalDayEmployeePresent += employeeAttendence;
+
+    //Checking if the employee is present or not.
+
+    switch(employeeAttendence){
+        case 1: {
+            if(checkPartForTime()) checkNumberOfPartTime += 1;
+            else checkNumberOfFullTime += 1;
+        } 
+        break;
+        case 0:{
+            console.log("Employee is Absent"+`on day ${i}`);
+        }
+        break;
+    }
 }
