fab802300dfb66cce5b209eca82b555a14579f55
Nachiketha <nachiketh.k.c@brainsightai.com>
2024-12-20 15:58:40 +0530

    total wage calculation

--- EmployeeWageCalculator.js
+++ EmployeeWageCalculator.js
@@ -2,10 +2,12 @@ console.log("Welcome To Employee Wage Calculator");
 
 
 let getEmployeeAttendence = () => Math.floor(Math.random() * 10) % 2;
-let totalDayEmployeePresent = 0;
-let checkNumberOfPartTime = 0;
-let checkNumberOfFullTime = 0;
 
+let checkPartForTime = () => Math.floor(Math.random() * 10) % 2;
+
+let totalDayEmployeePresent = 0;
+
+let checkNumberOfPartTime = 0;
 
 
 for (let i = 1; i <= 20; i++) {
@@ -15,15 +17,30 @@ for (let i = 1; i <= 20; i++) {
 
     //Checking if the employee is present or not.
 
-    switch(employeeAttendence){
+    switch (employeeAttendence) {
         case 1: {
-            if(checkPartForTime()) checkNumberOfPartTime += 1;
-            else checkNumberOfFullTime += 1;
-        } 
-        break;
-        case 0:{
-            console.log("Employee is Absent"+`on day ${i}`);
+            if (checkPartForTime()) {
+                checkNumberOfPartTime += 1
+                console.log(`Daily Wage of the Employee is ${20 * 4}`);
+
+            } else {
+                console.log(`Daily Wage of the Employee is ${20 * 8}`);
+            }
         }
-        break;
+            break;
+        case 0: {
+            console.log("Employee is Absent" + `on day ${i}`);
+        }
+            break;
     }
 }
+
+function getTotalWage() {
+    let totalDayFullTime = totalDayEmployeePresent - checkNumberOfPartTime;
+
+    let totalWage = (totalDayFullTime * 20 * 8) + (checkNumberOfPartTime * 20 * 4);
+    
+    return totalWage;
+}
+
+console.log(`Total Monthly Wage of the Employee is ${getTotalWage()}`);
